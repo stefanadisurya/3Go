1c8a09cc1edeaa3cfda1df6c3bb6f0cd99cd8b42
Fiona Stefani Limin <fiona16ti@alumni.pcr.ac.id>
2021-04-08 20:45:59 +0700

    Desain Profile View

--- ProfileViewController.swift
+++ ProfileViewController.swift
@@ -9,13 +9,36 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController{
+
+    @IBOutlet weak var ProfileTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Profile"
+        
+        ProfileTableView.delegate = self
+        ProfileTableView.dataSource = self
+        
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 89))
+        let footer = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 300))
+        //footer.backgroundColor = .orange
+        header.backgroundColor = .green
+        
+        ProfileTableView.tableHeaderView = header
+        ProfileTableView.tableFooterView = footer
+        
+        let label = UILabel()
+        label.frame = CGRect.init(x: 10, y: 5, width: 100, height: 35)
+        label.text = "Guest"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .yellow
+        header.addSubview(label)
+        
         // Do any additional setup after loading the view.
     }
+    
+
     
 
     /*
@@ -28,4 +51,36 @@ class ProfileViewController: UIViewController {
     }
     */
 
+}
+
+extension ProfileViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Summary"
+        cell.imageView?.image = UIImage( systemName:"star.square.fill")
+        cell.imageView?.tintColor = .systemYellow
+       
+        return cell
+    }
+    
+
+    
 }
