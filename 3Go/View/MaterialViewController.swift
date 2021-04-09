fe3e30166f27dbdf4be4b345b427782a5c530184
Yuliana Aulele <yulianaaulele@Yulianas-MacBook-Pro.local>
2021-04-09 09:34:28 +0700

    woho pages

---
+++ 3Go/View/MaterialViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MaterialViewController.swift
+//  3Go
+//
+//  Created by Yuliana Aulele on 08/04/21.
+//
+
+import UIKit
+
+class MaterialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Data")
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var wohoLabel: UILabel!
+    @IBOutlet weak var selamatLabel: UILabel!
+    @IBOutlet weak var exerciseButton: UIView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
