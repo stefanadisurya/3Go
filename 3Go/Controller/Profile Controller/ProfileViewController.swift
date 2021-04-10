f15f475ba93a5b6ce6326957cad9b688ad154a7f
Stefan Adisurya <stefanadisurya@gmail.com>
2021-04-10 15:50:20 +0700

    add material page, and profile page

---
+++ 3Go/Controller/Profile Controller/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  3Go
+//
+//  Created by Stefan Adisurya on 10/04/21.
+//
+
+import Foundation
---
+++ 3Go/Model/Material.swift
@@ -0,0 +1,8 @@
+//
+//  Material.swift
+//  3Go
+//
+//  Created by Stefan Adisurya on 10/04/21.
+//
+
+import Foundation
