e281faf723bd6fc2643fdda916c663cd2400eab6
Jackie Leonardy <jackieleonardy@Jackies-MacBook-Pro.local>
2021-04-14 19:42:50 +0700

    Add explanation changes

--- 3Go/Controller/Exercise Completion Controller/ExerciseFinishExplanationViewController.swift
+++ 3Go/Controller/Exercise Completion Controller/ExerciseFinishExplanationViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ExerciseFinishExplanationViewController: UIViewController {
     
+    @IBOutlet weak var explanationLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
